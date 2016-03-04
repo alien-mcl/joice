ff5a4d593d395caef9e0957e8026c8cf2fcfad1e
Karol Szczepański <alien-mcl@users.noreply.github.com>
2016-03-04 22:42:01 +0100

    And another version change.

---
+++ dist/grunt-joice-annotate.js
@@ -0,0 +1,36 @@
+﻿/*global __dirname*/
+module.exports = function(grunt) {
+    "use strict";
+    var path = require("path");
+    grunt.registerMultiTask("joiceAnnotate", "Annotates classes so they can be used in JoICe dependency injection framework after minification.", function () {
+        debugger;
+        var joice = require(__dirname + path.sep + "joice-annotate.js");
+        var codeProcessor = new joice.CodeProcessor();
+        this.files.forEach(function(file) {
+            var isExpandedPair = file.orig.expand || false;
+            file.src.filter(function(filePath) {
+                if (!grunt.file.exists(filePath)) {
+                    grunt.warn("Source file \"" + filePath + "\" not found.");
+                    return false;
+                }
+                
+                return true;
+            }).
+            map(function(filePath) {
+                if (grunt.file.isDir(filePath)) {
+                    return;
+                }
+                
+                var dest = file.dest;
+                if (grunt.util._.endsWith(dest, "/") === "directory") {
+                    dest = (isExpandedPair ? dest : path.join(dest, file.src));
+                }
+                
+                
+                var fileContent = grunt.file.read(filePath);
+                fileContent = codeProcessor.process(fileContent);
+                grunt.file.write(dest, fileContent);
+            });
+        });
+    });
+};
